aa81ccbadf8954943b1aa6e55f39f7f5db7a9202
emilesoub soub <emile.soubeyrand@api-vente.fr>
2025-07-26 09:01:39 +0000

    chore: saving work before workspace restart

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+// src/pages/HomePage.jsx
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenue sur l'outil de co-construction des Ruches !</h1>
+      <p>Le développement du MVP est en cours.</p>
+    </div>
+  );
+}
+
+export default HomePage;
